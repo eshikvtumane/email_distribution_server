d035aa4a97170d360fef29ea5125198242ff659a
eshikvtumane <pavel.tolstolytko@yandex.ru>
2018-06-22 09:36:30 +1000

    Add verification hash for unsubscribe and subscribe flag.

--- emails/submodels/email.py
+++ emails/submodels/email.py
@@ -1,10 +1,20 @@
+import random
+
 from django.db import models
 from django.db.models import ForeignKey, EmailField
 
 
+def create_hash():
+    return "%032x" % random.getrandbits(128)
+
+
 class Email(models.Model):
+
     email = EmailField()
     group = ForeignKey('emails.GroupEmail')
+
+    verification_hash = models.CharField(max_length=32, default=create_hash, unique=True)
+    subscription = models.BooleanField(default=True)
 
     def __str__(self):
         return self.email
