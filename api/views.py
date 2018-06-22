f19fb527052cbafdcd8870c125c45b0415499d19
eshikvtumane <pavel.tolstolytko@yandex.ru>
2018-06-22 22:21:23 +1000

    Add allow methods for rest api endpoints.

--- api/views.py
+++ api/views.py
@@ -9,13 +9,16 @@ from emails.submodels.group_email import GroupEmail
 class GroupEmailViewSet(viewsets.ModelViewSet):
     queryset = GroupEmail.objects.all()
     serializer_class = GroupEmailSerializer
+    http_method_names = ['get', 'post', 'delete']
 
 
 class EmailViewSet(viewsets.ModelViewSet):
     queryset = Email.objects.all()
     serializer_class = EmailSerializer
+    http_method_names = ['get', 'post', 'delete']
 
 
 class PeriodicTaskViewSet(viewsets.ModelViewSet):
     queryset = PeriodicTask.objects.all()
     serializer_class = PeriodicTaskSerializer
+    http_method_names = ['get', 'post', 'delete']
