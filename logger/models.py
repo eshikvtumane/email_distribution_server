84fb7dd27c9a4bc52b00212c3db34c54d678c91f
eshikvtumane <pavel.tolstolytko@yandex.ru>
2018-06-22 20:40:33 +1000

    Add model for logging email sender task

--- logger/models.py
+++ logger/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from enum import Enum
 
-# Create your models here.
+from django.db import models
+from django.db.models import fields
+
+
+class Status(Enum):
+    SUCCESS = 'success'
+    FAILURE = 'failure'
+
+    @classmethod
+    def choices(cls):
+        return tuple((x.name, x.value) for x in cls)
+
+
+class EmailSenderLogger(models.Model):
+    message = fields.TextField()
+    error = fields.TextField()
+    status = fields.CharField(max_length=255, choices=Status.choices(), default=Status.SUCCESS.name)
+    datetime_end = fields.DateTimeField(auto_now_add=True)
