ba085c980252faf27bbca367ff7884f3488fa7da
eshikvtumane <pavel.tolstolytko@yandex.ru>
2018-06-22 20:41:20 +1000

    Add logging model in admin form

--- logger/admin.py
+++ logger/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from logger.models import EmailSenderLogger
+
+
+class EmailSenderLoggerAdmin(admin.ModelAdmin):
+    readonly_fields = ('datetime_end',)
+    list_display = ('datetime_end', 'status',)
+    list_filter = ('status',)
+
+admin.site.register(EmailSenderLogger, EmailSenderLoggerAdmin)
