30950fc85409e952485728cd32a37c19d6dfe2d6
eshikvtumane <pavel.tolstolytko@yandex.ru>
2018-06-22 22:01:09 +1000

    Add celery task name. Add exceptions. Override representation (kwargs values moved in root).

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,13 @@
 import json
 
+from django.db import transaction
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
 from rest_framework import serializers
 from rest_framework.exceptions import APIException
 
 from emails.submodels.email import Email
 from emails.submodels.group_email import GroupEmail
+from helpers.celery_tasks import CeleryTasks
 
 
 class GroupEmailSerializer(serializers.ModelSerializer):
@@ -30,6 +32,7 @@ class PeriodicTaskSerializer(serializers.ModelSerializer):
     interval = IntervalScheduleSerializer()
     group_emails = serializers.IntegerField(required=False, write_only=True)
     kwargs = serializers.ModelField(model_field=PeriodicTask()._meta.get_field('kwargs'), read_only=True)
+    task = serializers.ModelField(model_field=PeriodicTask()._meta.get_field('task'), read_only=True)
 
     class Meta:
         model = PeriodicTask
@@ -44,20 +47,56 @@ class PeriodicTaskSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-        interval = validated_data.get('interval', None)
-        if interval is not None:
-            interval_obj = IntervalSchedule.objects.create(**interval)
-            validated_data['interval'] = interval_obj
-        else:
-            raise APIException('Interval parameter empty')
+        """
+        Create periodic task and interval scheduler from parameters.
+        Paraneter group_emails write in kwarg attribute an remove from dict.
+        :param validated_data:
+        :return:
+        """
+        interval_key = 'interval'
+        group_emails_key = 'group_emails'
+        task_key = 'task'
+        kwargs_key = 'kwargs'
 
-        group_emails = validated_data.pop('group_emails', None)
-        if group_emails is not None:
-            validated_data['kwargs'] = json.dumps({'group_emails': group_emails})
-        else:
-            raise APIException('Group emeils parameter empty')
+        try:
+            with transaction.atomic():
+                interval = self._get_value_by_key(validated_data, interval_key, 'Interval parameter empty')
+                interval_obj = IntervalSchedule.objects.create(**interval)
+                validated_data[interval_key] = interval_obj
 
-        return super(PeriodicTaskSerializer, self).create(validated_data)
+                group_emails = self._get_value_by_key(validated_data, group_emails_key, 'Group emails parameter empty')
+                validated_data[kwargs_key] = json.dumps({group_emails_key: group_emails})
+                self._remove_value_from_dict_by_key(group_emails_key, validated_data)
 
-    def get_group_emails(self):
-        pass
+                validated_data[task_key] = CeleryTasks.EMAIL_SENDER.value
+                result = super(PeriodicTaskSerializer, self).create(validated_data)
+                return result
+        except Exception as e:
+            raise APIException(str(e))
+
+
+    def _get_value_by_key(self, data_dict, key, exception_error):
+        value = data_dict.get(key, None)
+        if value is None:
+            raise APIException(exception_error)
+        return value
+
+    def _remove_value_from_dict_by_key(self, key, data_dict):
+        data_dict.pop(key, None)
+
+    def to_representation(self, instance):
+        """
+        Add group_emails parameter from kwargs attribute
+        :param instance:
+        :return:  dict
+        """
+        result = super(PeriodicTaskSerializer, self).to_representation(instance)
+
+        try:
+            kwargs = json.loads(result.pop('kwargs', None))
+        except:
+            raise APIException('Task kwargs parameters contains not in dictionary type.')
+
+        result = {**result, **kwargs}
+        return result
+
