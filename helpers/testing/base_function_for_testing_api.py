ccabdd0df416d1433c814f3e4f90e28163977e1e
eshikvtumane <pavel.tolstolytko@yandex.ru>
2018-06-24 17:41:29 +1000

    Create base class for tests.

--- helpers/testing/base_function_for_testing_api.py
+++ helpers/testing/base_function_for_testing_api.py
@@ -1,3 +1,5 @@
+from urllib import parse
+
 from mixer.backend.django import mixer
 from rest_framework.test import APIClient
 import pytest
@@ -8,3 +10,25 @@ from helpers.testing.locale_fixture import *
 @pytest.fixture
 def api_client():
     return APIClient()
+
+
+class BaseTest:
+    url_client = None
+
+    def setup_class(self):
+        self.api_client = APIClient()
+
+    def url_join(self, *args):
+        args = [str(arg) for arg in args]
+        url = parse.urljoin(*args) + '/'
+        return url
+
+    def get(self, url, format='json'):
+        return self.api_client.get(url, format=format)
+
+    def post(self, url, data={}, format='json'):
+        return self.api_client.post(url, data, format='json')
+
+    def delete(self, url):
+        return self.api_client.delete(url)
+
