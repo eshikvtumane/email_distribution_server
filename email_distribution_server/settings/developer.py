470476a9b83d33c22a69ed867aad1bd680835c61
eshikvtumane <pavel.tolstolytko@yandex.ru>
2018-06-24 21:26:39 +1000

    Add localhost in allow_hosts

--- email_distribution_server/settings/developer.py
+++ email_distribution_server/settings/developer.py
@@ -6,7 +6,7 @@ SECRET_KEY = '+5)%qf)z4475va96o%$ek5yo_*lyd9dc%6e35u&4h_l)@dm%m8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['testserver', '127.0.0.1']
+ALLOWED_HOSTS = ['testserver', '127.0.0.1', 'localhost']
 
 
 # Database
